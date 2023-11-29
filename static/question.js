3a34cbf47b220ce4c5f3a8962aa90e0de97d5505
harshit <harshitwadhwani23@gmail.com>
2023-11-29 18:17:35 +0530

    Co-authored-by: Sejal Kaur Virdi <Kesu-1407@users.noreply.github.com>

--- static/question.js
+++ static/question.js
@@ -4,19 +4,31 @@ let recordedChunks = [];
 let questionIndex = 0;
 const questions = [];
 
-fetch('questions_audio/')
+fetch('../questions_audio/')
   .then(response => response.text())
   .then(data => {
     const parser = new DOMParser();
     const htmlDocument = parser.parseFromString(data, 'text/html');
-    const links = htmlDocument.querySelectorAll('a');
-    links.forEach(link => {
-      if (link.href.endsWith('.mp3')) {
-        questions.push(link.href.split('/').pop());
-      }
-    });
+    
+    // Assuming your links are stored in an element with the ID 'audioLinks'
+    const linksContainer = htmlDocument.getElementById('audioLinks');
+    
+    if (linksContainer) {
+      const links = linksContainer.querySelectorAll('a');
+      
+      links.forEach(link => {
+        if (link.href.endsWith('.mp3')) {
+          questions.push(link.href);
+        }
+      });
+      
+      console.log('Fetched questions:', questions);
+    } else {
+      console.error('Audio links container not found');
+    }
   })
   .catch(error => console.error('Error fetching questions:', error));
+
 // const questions = [
 //     'question1.mp3',
 //     'question2.mp3',
@@ -30,7 +42,14 @@ const questionAudio = document.getElementById('questionAudio');
 function startInterview() {
     if (questionIndex < questions.length) {
         questionAudio.src = questions[questionIndex];
-        questionAudio.play();
+        console.log('Audio source:', questionAudio.src);
+        questionAudio.play()
+        .then(() => {
+          console.log('Audio playback started successfully');
+        })
+        .catch(error => {
+          console.error('Error starting audio playback:', error);
+        });
         questionIndex++;
     } else {
         alert('Interview completed!');
@@ -48,6 +67,14 @@ function startRecording() {
             };
 
             alert('Recording started...');
+            mediaRecorder.onstop = function(e) {
+              console.log('Recording stopped:', e);
+          };
+          
+          mediaRecorder.onerror = function(e) {
+              console.error('MediaRecorder error:', e);
+          };
+          
         })
         .catch(err => console.log('getUserMedia Error: ', err));
 }
@@ -63,9 +90,9 @@ function saveAnswer() {
     const a = document.createElement('a');
     a.style.display = 'none';
     a.href = url;
-    a.download = `${email}:ans${questionIndex}.mp3`;
+    a.download = `${email}:ans${questionIndex}.wav`;
     document.body.appendChild(a);
-    a.setAttribute('download', `answer_audio/${email}:ans${questionIndex}.mp3`);
+    a.setAttribute('download', `answer_audio/${email}:ans${questionIndex}.wav`);
     document.body.appendChild(a);
     a.click();
     alert('Answer saved!');
